37f01ef7b6a24af771662821366f3301570ce46e
Pei Li <flyiei.jason@gmail.com>
2023-07-04 08:33:55 +0200

    main: paging working

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ import api from './api/reservations';
 
 function App() {
 
+const [totalPages, setTotalPages] = useState(0);
 const [pageNum, setPageNum] = useState(0);
-const [pageSize, setPageSize] = useState(5);
+const [pageSize, setPageSize] = useState(4);
 
 const [reservations, setReservations] = useState([]);
 const [agency, setAgency] = useState('');
@@ -23,31 +24,59 @@ const [editHotel, setEditHotel] = useState('');
 
 const history = useHistory();
 
-useEffect(()=>{
-  const fetchReservations = async () => {
-    try {
-        const response = await api.get('/reservations', {
-          params: {
-            pageNum: pageNum,
-            pageSize: pageSize
-          }
-        })
-        setReservations(response.data);
-    } catch (err) {
-        if (err.response){
-            // not in the 200 response range
-            console.log(err.response.data)
-            console.log(err.response.status)
-            console.log(err.response.headers)
-        } else {
-            console.log(`Error: ${err.message}`)
-        } 
-    }
+const fetchReservations = async () => {
+  try {
+      const response = await api.get('/reservations', {
+        params: {
+          pageNum: pageNum,
+          pageSize: pageSize
+        }
+      })
+      setReservations(response.data);
+  } catch (err) {
+      if (err.response){
+          // not in the 200 response range
+          console.log(err.response.data)
+          console.log(err.response.status)
+          console.log(err.response.headers)
+      } else {
+          console.log(`Error: ${err.message}`)
+      } 
   }
+}
 
+const fetchTotalPages = async () => {
+  try {
+      const response = await api.get('/reservations/totalPages', {
+        params: {
+          pageSize: pageSize
+        }
+      })
+      setTotalPages(response.data);
+  } catch (err) {
+      if (err.response){
+          // not in the 200 response range
+          console.log(err.response.data)
+          console.log(err.response.status)
+          console.log(err.response.headers)
+      } else {
+          console.log(`Error: ${err.message}`)
+      } 
+  }
+}
+
+useEffect(()=>{
   fetchReservations();
-},[])
+  fetchTotalPages();
+},[pageNum])
 
+const handleNextPage = () => {
+  setPageNum(pageNum + 1);
+};
+
+const handlePreviousPage = () => {
+  setPageNum(pageNum - 1);
+};
 
 const handleSubmit = async (e) => {
   e.preventDefault();
@@ -62,6 +91,8 @@ const handleSubmit = async (e) => {
     setAgency('');
     setConsumer('');
     setHotel('');
+    fetchReservations();
+    fetchTotalPages();
     history.push('/');
   } catch (err) {
     console.log(`Error: ${err.message}`)
@@ -93,6 +124,13 @@ const handleDelete = async (id) => {
     await api.delete(`/reservations/${id}`)
     const reservationsList = reservations.filter((reservation)=> reservation.id !== id);
     setReservations(reservationsList);
+    if (reservationsList.length === 0 && pageNum > 0) {
+      setPageNum(pageNum - 1) // pageNum update will trigger useEffect to fetch reservations from server
+    }else {
+      // fetching reservations from server to fill the empty slot 
+      fetchReservations();
+      fetchTotalPages();
+    }
     // accessing the browser history with react router, it will route to the home page
     // this is not requesting anything from the server
     history.push('/')
@@ -108,7 +146,13 @@ const handleDelete = async (id) => {
        <Switch>
             {/* without 'exact', '/reservation' and '/*' will also go to 'Home' component */}
             <Route exact path='/'>
-                <Home reservations={reservations}/>
+                <Home reservations={reservations} 
+                    totalPages={totalPages} 
+                    pageNum={pageNum} 
+                    setPageNum={setPageNum}
+                    pageSize={pageSize}
+                    handleNextPage={handleNextPage}
+                    handlePreviousPage={handlePreviousPage}/>
             </Route>
             <Route exact path='/reservation'>
                 <NewReservation 
--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,20 @@
 import Content from "./Content"
 import {Link } from "react-router-dom"
 
-const Home = ({reservations}) => {
+const Home = ({reservations, totalPages, pageNum, setPageNum, pageSize, handleNextPage, handlePreviousPage}) => {
     return (
       <main>
           <h2><Link to="/reservation">Create Reservation</Link></h2>
           <h2>Reservation List</h2>
           {reservations.length ? 
-                (<Content reservations={reservations}/>) 
+                (
+                  <dev>
+                    <Content reservations={reservations}/>
+                    <p>Total {totalPages} pages; Yon are on page -- {pageNum+1} -- ; Page Size: {pageSize}</p>
+                    {(pageNum > 0) && (<button onClick={handlePreviousPage}>Previous Page</button>)}
+                    {(pageNum + 1 < totalPages) && (<button onClick={handleNextPage}>Next Page</button>)}
+                  </dev>
+                ) 
                 : (<p> No Reservations to display </p>)}
       </main>
     )
